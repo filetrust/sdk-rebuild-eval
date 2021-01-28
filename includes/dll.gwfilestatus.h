cc7cad554e4b3a849989bb3abd4f71feb0a0930e
Build Service <buildagent@glasswallsolutions.com>
2021-01-28 13:18:28 +0000

    Update of libs to 1.148

--- includes/dll.gwfilestatus.h
+++ includes/dll.gwfilestatus.h
@@ -11,7 +11,7 @@
 *   The document processing functions use the enumerations defined on these pages to describe the processing results. \n
 *   \n
 *   For the @ref gwFileProcessingFileToMemory functions only ::eGwFileStatus_Success,  ::eGwFileStatus_Error and ::eGwFileStatus_InternalError are used.
-*   For the @ref gwFileProcessingFileToFile functions the negative values are utilised to report issues with file output (possibliy in addition to file processing issues)
+*   For the @ref gwFileProcessingFileToFile functions the negative values are utilised to report issues with file output (possibly in addition to file processing issues)
 *   @attention Some of the @ref gwFileProcessingFileToFile functions' negative return values indicate documents have been successfully managed. It may be additional outputs that have
 *   caused the error condition to be reported. Care should be taken to make the managed document available if possible.
 *
